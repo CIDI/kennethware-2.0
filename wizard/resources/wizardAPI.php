2d09255d188e88f2f9dd607c87c5c1cefb50c486
Kenneth <kenneth.larsen@usu.edu>
2014-07-07 11:27:22 -0600

    http_parse_headers function update
    Part of previous function used deprecated code. Also wrapped with check
    for existing function

--- wizard/resources/wizardAPI.php
+++ wizard/resources/wizardAPI.php
@@ -22,20 +22,36 @@
 
 
 	// the following functions run the GET and POST calls
-		function http_parse_headers( $header ) {
-		    $retVal = array();
-		    $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
-		    foreach( $fields as $field ) {
-		        if( preg_match('/([^:]+): (.+)/m', $field, $match) ) {
-		            $match[1] = preg_replace('/(?<=^|[\x09\x20\x2D])./e', 'strtoupper("\0")', strtolower(trim($match[1])));
-		            if( isset($retVal[$match[1]]) ) {
-		                $retVal[$match[1]] = array($retVal[$match[1]], $match[2]);
-		            } else {
-		                $retVal[$match[1]] = trim($match[2]);
+		if (!function_exists('http_parse_headers')) {
+		    function http_parse_headers($raw_headers) {
+		        $headers = array();
+		        $key = '';
+
+		        foreach(explode("\n", $raw_headers) as $i => $h) {
+		            $h = explode(':', $h, 2);
+
+		            if (isset($h[1])) {
+		                if (!isset($headers[$h[0]]))
+		                    $headers[$h[0]] = trim($h[1]);
+		                elseif (is_array($headers[$h[0]])) {
+		                    $headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1])));
+		                }
+		                else {
+		                    $headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1])));
+		                }
+
+		                $key = $h[0];
+		            }
+		            else { 
+		                if (substr($h[0], 0, 1) == "\t")
+		                    $headers[$key] .= "\r\n\t".trim($h[0]);
+		                elseif (!$key) 
+		                    $headers[0] = trim($h[0]); 
 		            }
 		        }
+
+		        return $headers;
 		    }
-	    	return $retVal;
 		}
 		function curlGet($url) {
 			global $token;
