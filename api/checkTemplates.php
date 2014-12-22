f153adee720353949a298b2b8005989e7148ea65
Kenneth <kenneth.larsen@usu.edu>
2014-12-22 14:00:28 -0700

    API Pages Tweaks
    Created a config file specific to the API pages for the tools as part
    of an effort to reduce duplicate files over multiple Canvas instances.
    Also commented out Access-Control-Allow-Origin. Remove comment if you
    need this aspect

--- api/checkTemplates.php
+++ api/checkTemplates.php
@@ -1,7 +1,7 @@
 <?php
 	require_once 'canvasAPI.php';
-	require_once (__DIR__.'/../config.php');
-	header('Access-Control-Allow-Origin: '.$canvasDomain);
+	require_once 'config.php';
+	// header('Access-Control-Allow-Origin: '.$canvasDomain);
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 	$courseID = $_POST['courseID'];
